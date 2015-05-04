e80607baeedb85af8c33b1e7b35cf63189eb98f3
Eddy00707 <EdwardMuzhevskiy@gmail.com>
2015-05-04 19:52:55 +0300

    A large change
    Over 9000 menus, buttons, etc. Including Main Menu and Pause menu.
    Almost everything is placeholders

---
+++ Student Simulator/Assets/UI/UI.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UI : MonoBehaviour
+{
+	public GameObject gameUI;
+	public GameObject PauseMenu;
+
+	public bool pauseMenuIsShowing;
+
+	public void QuitGame()
+	{
+		Application.Quit();
+		return;
+	}
+
+	// Use this for initialization
+	void Start () 
+	{
+
+	}
+
+	public void ShowPauseMenu()
+	{
+		pauseMenuIsShowing=!pauseMenuIsShowing;
+		PauseMenu.SetActive(pauseMenuIsShowing);
+		gameUI.SetActive(!pauseMenuIsShowing);
+		if(pauseMenuIsShowing)Time.timeScale=0;
+		else Time.timeScale=1;
+		return;
+	}
+
+
+	// Update is called once per frame
+	void Update () 
+	{
+
+		if(Input.GetKeyDown(KeyCode.Escape))
+		{
+			ShowPauseMenu ();
+		}
+	}
+}
