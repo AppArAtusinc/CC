a5600e02bf7819d4f8dbcb7f60921748ed7c5fc2
Eddy00707 <EdwardMuzhevskiy@gmail.com>
2015-05-09 20:20:26 +0300

    some things as well

--- Student Simulator/Assets/UI/UI.cs
+++ Student Simulator/Assets/UI/UI.cs
@@ -5,8 +5,10 @@ public class UI : MonoBehaviour
 {
 	public GameObject gameUI;
 	public GameObject PauseMenu;
-
-	public bool pauseMenuIsShowing;
+	public GameObject MainMenu;
+	public GameObject PlayerMenu;
+	public int TimeSpeed=1;
+	private bool pauseMenuIsShowing, mainMenuIsShowing, playerMenuIsShowing;
 
 	public void QuitGame()
 	{
@@ -17,27 +19,68 @@ public class UI : MonoBehaviour
 	// Use this for initialization
 	void Start () 
 	{
-
+		pauseMenuIsShowing = false;
+		mainMenuIsShowing = false;
+		playerMenuIsShowing = false;
+		PauseMenu.SetActive(pauseMenuIsShowing);
+		MainMenu.SetActive(mainMenuIsShowing);
+		PlayerMenu.SetActive(playerMenuIsShowing);
+	}
+	public void ShowPlayerMenu()
+	{
+		if(!pauseMenuIsShowing&&!mainMenuIsShowing)
+		{
+			playerMenuIsShowing=!playerMenuIsShowing;
+			PlayerMenu.SetActive(playerMenuIsShowing);
+		}
+		return;
 	}
 
 	public void ShowPauseMenu()
 	{
 		pauseMenuIsShowing=!pauseMenuIsShowing;
 		PauseMenu.SetActive(pauseMenuIsShowing);
-		gameUI.SetActive(!pauseMenuIsShowing);
-		if(pauseMenuIsShowing)Time.timeScale=0;
-		else Time.timeScale=1;
 		return;
 	}
+	public void ShowGameUI()
+	{
+		mainMenuIsShowing=!mainMenuIsShowing;
+		MainMenu.SetActive(mainMenuIsShowing);
+		gameUI.SetActive(!mainMenuIsShowing);
+	}
 
+	public void ShowMainMenu()
+	{
+		ShowPauseMenu();
+		ShowGameUI ();
+
+		return;
+	}
 
 	// Update is called once per frame
 	void Update () 
 	{
+		if(pauseMenuIsShowing||mainMenuIsShowing||playerMenuIsShowing)
+		{
+			Time.timeScale=0;
+			Cursor.lockState = CursorLockMode.None;
+			Cursor.visible = true;
 
-		if(Input.GetKeyDown(KeyCode.Escape))
+		}
+		else //Normal gameplay 
+		{
+			Time.timeScale=TimeSpeed;
+
+			Cursor.lockState = CursorLockMode.Locked; //устанавливает курсор в центре и запрещает перемещиние
+			Cursor.visible = false;
+		}
+		if(Input.GetKeyDown(KeyCode.Escape)&&!mainMenuIsShowing)
 		{
 			ShowPauseMenu ();
+		}
+		if(Input.GetKeyDown(KeyCode.Tab)&&!mainMenuIsShowing)
+		{
+			ShowPlayerMenu ();
 		}
 	}
 }
