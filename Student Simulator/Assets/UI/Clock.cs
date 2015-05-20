45bde3e2584bf01407dd507712eb759fe7b747db
Eddy00707 <EdwardMuzhevskiy@gmail.com>
2015-05-20 20:18:26 +0300

    Final

---
+++ Student Simulator/Assets/UI/Clock.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using UnityEngine.UI;
+
+public class Clock : MonoBehaviour 
+{
+	DateTime time;
+	// Use this for initialization
+	void Start () 
+	{
+
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		time = DateTime.Now;
+		Text str = GetComponent<Text>();
+		string hour = time.Hour.ToString ();
+		string minute = time.Minute.ToString();
+		if(int.Parse (hour)<10)
+		{
+			hour="0"+hour;
+		}
+		if(int.Parse (minute)<10)
+		{
+			minute="0"+minute;
+		}
+		str.text = hour+":"+minute;
+
+	}
+}
