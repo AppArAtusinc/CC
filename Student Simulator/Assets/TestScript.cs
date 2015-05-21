2b8819ffb56afb78842c8c1a70c657406d33b03f
Eddy00707 <EdwardMuzhevskiy@gmail.com>
2015-05-21 18:39:31 +0300

    Buttons Save\load

--- Student Simulator/Assets/TestScript.cs
+++ Student Simulator/Assets/TestScript.cs
@@ -32,48 +32,65 @@ public class TestScript : MonoBehaviour {
 		}
 
 		//saving
-		var saveableObjects = GameObject.FindGameObjectsWithTag("Saveable");
-		SaveManager saveManager = new SaveManager("test_save_data.xml");
-		SceneState saveScene = new SceneState();
+
 		
 		//save
-		if(Input.GetKeyDown(KeyCode.C)){			
-			for(int i = 0; i<saveableObjects.Length; i++){
-				var cube1PosX = saveableObjects[i].transform.position.x;
-				var cube1PosY = saveableObjects[i].transform.position.y;
-				var cube1PosZ = saveableObjects[i].transform.position.z;
-				
-				var cube1AngleX = saveableObjects[i].transform.rotation.x;
-				var cube1AngleY = saveableObjects[i].transform.rotation.y;
-				var cube1AngleZ = saveableObjects[i].transform.rotation.z;
-				var cube1AngleW = saveableObjects[i].transform.rotation.w;
-				
-				SaveObject sObjCube = new SCube("Test Cube", cube1PosX, cube1PosY, cube1PosZ, cube1AngleX, cube1AngleY, cube1AngleZ, cube1AngleW);
-				
-				saveScene.AddItem(sObjCube);
-			}
-
-			saveManager.AddInSaveList(saveScene);
-
-			saveManager.SaveAll();		
+		if(Input.GetKeyDown(KeyCode.C))
+		{			
+			SaveScene ();	
 		}
 
 		//load
-		if(Input.GetKeyDown(KeyCode.L)){
-			var listOfSavedObjects = saveManager.LoadAll();
-
-			for(int j = 0; j<listOfSavedObjects.Count; j++){
-				var objName = listOfSavedObjects[j].objectName;
-				Vector3 objPos = new Vector3(listOfSavedObjects[j].x, listOfSavedObjects[j].y, listOfSavedObjects[j].z);
-				Quaternion objRotation = new Quaternion(listOfSavedObjects[j].rotationX, listOfSavedObjects[j].rotationY, listOfSavedObjects[j].rotationZ, listOfSavedObjects[j].rotationW);
-
-				var resourceObj = Resources.Load("Objects/" + objName);
-
-				GameObject.Instantiate(resourceObj, objPos, objRotation);
-			}
+		if(Input.GetKeyDown(KeyCode.L))
+		{
+			LoadScene ();
 		}
 
 	}
+
+	public void LoadScene()
+	{
+		SaveManager saveManager = new SaveManager("test_save_data.xml");
+		var listOfSavedObjects = saveManager.LoadAll();
+		
+		for(int j = 0; j<listOfSavedObjects.Count; j++){
+			var objName = listOfSavedObjects[j].objectName;
+			Vector3 objPos = new Vector3(listOfSavedObjects[j].x, listOfSavedObjects[j].y, listOfSavedObjects[j].z);
+			Quaternion objRotation = new Quaternion(listOfSavedObjects[j].rotationX, listOfSavedObjects[j].rotationY, listOfSavedObjects[j].rotationZ, listOfSavedObjects[j].rotationW);
+			
+			var resourceObj = Resources.Load("Objects/" + objName);
+			
+			GameObject.Instantiate(resourceObj, objPos, objRotation);
+		}
+	}
+
+
+
+	public void SaveScene()
+	{
+		var saveableObjects = GameObject.FindGameObjectsWithTag("Saveable");
+		SaveManager saveManager = new SaveManager("test_save_data.xml");
+		SceneState saveScene = new SceneState();
+		for(int i = 0; i<saveableObjects.Length; i++){
+			var cube1PosX = saveableObjects[i].transform.position.x;
+			var cube1PosY = saveableObjects[i].transform.position.y;
+			var cube1PosZ = saveableObjects[i].transform.position.z;
+			
+			var cube1AngleX = saveableObjects[i].transform.rotation.x;
+			var cube1AngleY = saveableObjects[i].transform.rotation.y;
+			var cube1AngleZ = saveableObjects[i].transform.rotation.z;
+			var cube1AngleW = saveableObjects[i].transform.rotation.w;
+			
+			SaveObject sObjCube = new SCube("Test Cube", cube1PosX, cube1PosY, cube1PosZ, cube1AngleX, cube1AngleY, cube1AngleZ, cube1AngleW);
+			
+			saveScene.AddItem(sObjCube);
+		}
+		
+		saveManager.AddInSaveList(saveScene);
+		
+		saveManager.SaveAll();
+	}
+
 	// Update is called once per frame
 	void Update () {
 
